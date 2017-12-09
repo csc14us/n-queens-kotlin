114f62ec9516f6a870ac7a91e43244bf1bb8754d
csc14us <curtis.s.cooper@gmail.com>
2017-12-09 18:47:46 -0500

    Add files via upload

---
+++ FixedByteStack.kt
@@ -0,0 +1,109 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 Curtis S. Cooper
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package us.csc.queens
+
+import java.util.*
+
+val EMPTY_BYTE_ARRAY = byteArrayOf()
+
+/**
+ * A fixed-sized stack of Byte integer values used by the N-queens solver.
+ *
+ * Written to avoid the overhead of boxing and any dynamic-memory
+ * allocation that would be associated with a Collection.
+ */
+class FixedByteStack(val capacity: Int) {
+    private val backingArray = ByteArray(capacity)
+    private var currentIndex = -1
+
+    fun empty(): Boolean = (currentIndex < 0)
+    fun size(): Int = if (empty()) 0 else (currentIndex + 1)
+
+    /** Does not mutate the stack: returns a copy. */
+    fun toArray(): ByteArray {
+        if (empty()) {
+            return EMPTY_BYTE_ARRAY
+        }
+
+        val result = ByteArray(size())
+        for (index in 0..currentIndex) {
+            result[index] = backingArray[index]
+        }
+
+        return result
+    }
+
+    fun push(element: Byte) {
+        val nextIndex = (currentIndex + 1)
+        backingArray[nextIndex] = element
+        currentIndex = nextIndex
+    }
+
+    fun top(): Byte {
+        return backingArray[currentIndex]
+    }
+
+    fun pop(): Byte {
+        val top = top()
+        --currentIndex
+        return top
+    }
+
+    fun clear() {
+        currentIndex = -1
+        backingArray.fill(0)
+    }
+
+    override fun toString(): String {
+        return "FixedByteStack" + Arrays.toString(toArray())
+    }
+
+    override fun hashCode(): Int {
+        var hash: Int = Objects.hash(currentIndex)
+
+        for (i in 0..currentIndex) {
+            hash += Objects.hash(backingArray[i])
+        }
+
+        return hash
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is FixedByteStack) {
+            return false
+        }
+
+        if (other.size() != size()) {
+            return false
+        }
+
+        for (i in 0..currentIndex) {
+            if (other.backingArray[i] != backingArray[i]) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
---
+++ Runner.kt
@@ -0,0 +1,162 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 Curtis S. Cooper
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package us.csc.queens
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+private enum class ArgList(val cliArg: String) {
+    CORES("--cores"),
+    HELP("--help"),
+    MODE("--mode"),
+    SIZE("--size"),
+    VERSION("--version");
+}
+
+private val USAGE: String = """* N-Queens, v. $NQUEENS_VERSION
+Usage:
+  --cores <#>: specify the number of CPU cores; must be >= 1; the default is 1 if not specified.
+  --help: prints this help message and exits.
+  --mode <MODE>: specify the program mode.  Choices for the MODE parameter include:
+    benchmark - Count the solutions along with time spent; used to measure CPU performance.
+    first - Print the first solution and exit; useful to see a solution of very large boards.
+    trace: print a trace of the algorithm step-by-step, as well as the solutions.
+    view: print all solutions of the N-Queen problem for the given board size.
+  --size <#>: specify the size of the board; must be >= 1; the default is 8.
+  --version: prints the version of the program and exits.
+
+Example:
+  > java -jar bin/n-queens-$NQUEENS_VERSION.jar --mode benchmark --size 14
+  Counts all solutions to the N-Queens problem for a 14x14 chess board.
+"""
+
+fun main(args: Array<String>) {
+    if (args.isEmpty() || !args.contains(ArgList.MODE.cliArg)) {
+        println(USAGE)
+        return
+    }
+
+    var boardSize: Byte = REGULAR_CHESS_BOARD_SIZE
+    var numThreads = 1
+    var traceSteps = false
+    var printSolutions = false
+    var stopAfterFirst = false
+
+    var argIndex = 0
+    while (argIndex < args.size) {
+        val arg = args[argIndex].toLowerCase()
+        when (arg) {
+            ArgList.SIZE.cliArg -> try {
+                boardSize = args[++argIndex].toByte()
+            } catch (e: Throwable) {
+                println(USAGE)
+                return
+            }
+
+            ArgList.CORES.cliArg -> try {
+                numThreads = args[++argIndex].toInt()
+            } catch (e: Throwable) {
+                println(USAGE)
+                return
+            }
+
+            ArgList.MODE.cliArg -> try {
+                val mode = args[++argIndex].toLowerCase()
+                when (mode) {
+                    "benchmark" -> printSolutions = false
+                    "first" -> {
+                        stopAfterFirst = true
+                        printSolutions = true
+                    }
+                    "trace" -> {
+                        traceSteps = true
+                        printSolutions = true
+                    }
+                    "view" -> printSolutions = true
+                    else -> {
+                        println(USAGE)
+                        return
+                    }
+                }
+            } catch (e: Throwable) {
+                println(USAGE)
+                return
+            }
+
+            else -> when (arg) {
+                ArgList.HELP.cliArg -> {
+                    println(USAGE)
+                    return
+                }
+
+                ArgList.VERSION.cliArg -> {
+                    println("N-Queens, v. $NQUEENS_VERSION")
+                    return
+                }
+
+                else -> {
+                    println(USAGE)
+                    return
+                }
+            }
+        }
+        ++argIndex
+    }
+
+    runSolver(boardSize, numThreads, traceSteps, printSolutions, stopAfterFirst)
+}
+
+internal fun getTimeDateString(dateTime: LocalDateTime): String {
+    return dateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy hh:mm:ss a"))
+}
+
+private fun runSolver(boardSize: Byte,
+                      numThreads: Int,
+                      traceSteps: Boolean,
+                      printSolutions: Boolean,
+                      stopAfterFirst: Boolean) {
+    println(getTimeDateString(LocalDateTime.now()))
+    println("Solving a ${boardSize}x${boardSize} board; # threads = $numThreads:\n")
+
+    var solverResult = SolveResult()
+    run {
+        val startTime: Long = System.nanoTime()
+        solverResult = solveNQ(
+            boardSize = boardSize,
+            numThreads = numThreads,
+            printSteps = traceSteps,
+            firstSolutionOnly = stopAfterFirst,
+            collectSolutions = false,
+            printSolutions = printSolutions)
+        val endTime: Long = System.nanoTime()
+
+        val duration: Double = (endTime - startTime).toDouble() / 1.0e9
+        val durationStr = String.format("%.3g", duration)
+        println("\nTime to solve: $durationStr [s]")
+    }
+
+    println("Total solutions found = ${solverResult.numSolutions}")
+    println(getTimeDateString(LocalDateTime.now()))
+}
+ 
